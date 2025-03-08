ad2778c37ab7249fcccbbf92bccd26cc5a57be45
masudparvez2050 <masudparvez2050@gmail.com>
2025-03-08 20:28:59 +0000

    error fix

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,9 +2,10 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 // Initialize Stripe with your secret key
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2023-10-16",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "");
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
+//   apiVersion: "2023-10-16",
+// });
 
 export async function POST(req: Request) {
   try {
