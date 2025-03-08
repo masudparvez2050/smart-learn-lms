62ccd5982644b6ca60f5db3e066e3c2f9754992a
masudparvez2050 <masudparvez2050@gmail.com>
2025-03-08 20:31:21 +0000

    error fix

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2023-10-16",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "");
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
+//   apiVersion: "2023-10-16",
+// });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET || "";
 
